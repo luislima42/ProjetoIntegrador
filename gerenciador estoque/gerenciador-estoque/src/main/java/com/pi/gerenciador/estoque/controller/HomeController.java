b0a774e28f5ecdf06a9f42eeb8ec01ea66f85a72
Aluno <luis.lima42@fatec.sp.gov.br>
2023-03-15 11:06:59 -0300

    Projeto integrador, primeiro commit, estrutura basica

---
+++ gerenciador estoque/gerenciador-estoque/src/main/java/com/pi/gerenciador/estoque/controller/HomeController.java
@@ -0,0 +1,24 @@
+package com.pi.gerenciador.estoque.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+
+    @GetMapping("/dashboard")
+    public String dashboard(){
+        return "dashboard";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+}
---
+++ gerenciador estoque/gerenciador-estoque/src/test/java/com/pi/gerenciador/estoque/GerenciadorEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pi.gerenciador.estoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
